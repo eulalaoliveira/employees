8da2dc57b9eb70cc46f786d96c3e4fe527925332
Larissa Oliveira <larissa.brazil@live.com>
2023-02-04 22:54:06 +1100

    adding verifications

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+var table = document.getElementById("infoTable");
+var hidden = table.style.display = "none";
 var searchBtn = document.getElementById('searchBtn');
 
 searchBtn.onclick = function () {
+    
     var inputText = document.getElementById('inputText').value;
+
+    if (inputText) {
+        table.style.display = "";
+    } 
 
     if (inputText == "") {
         return alert("Type something.");
     } else {
-        
         fetch("employees.json").then((response) => {
             response.json().then((usuarios) => {
                 usuarios.map((usuario) => {
                     console.log("foi chamada");
-
-                    
                     if (usuario.empId == inputText ||
                     usuario.empLastname == inputText || 
                     usuario.empLevel == inputText) {
@@ -22,10 +26,11 @@ searchBtn.onclick = function () {
                         document.getElementById('lastNameTable').innerHTML = usuario.empLastname;
                         document.getElementById('salaryTable').innerHTML = usuario.empSalary;
                         document.getElementById('levelTable').innerHTML = usuario.empLevel;
-                        }
+                        } 
                 })
             })
         })
     }
-    
 }
+
+
