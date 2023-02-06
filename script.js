00f4ff6b7c06cbb96c00e721e68dd00cae2cd0d2
Larissa Oliveira <larissa.brazil@live.com>
2023-02-06 11:40:12 +1100

    filter

--- script.js
+++ script.js
@@ -9,10 +9,41 @@ searchBtn.onclick = async function () {
     //it takes the value of textarea - search value
     var inputText = document.getElementById('inputText').value;
 
-    //fetch to take json file and then create a array to be used
+    //fetch to take json file and then create an array to be used
     const data = await fetch("http://localhost:8000/", {params: { term: inputText }}).then((response) => response.json())
     let html = '';
-    (data || []).forEach(usuario => {
+
+    //filter by id, lastname and level
+    if (inputText.length >= 1) {
+      const result = data.filter(usuario => 
+        usuario.empId == inputText ||
+        usuario.empLastname == inputText || 
+        usuario.empLevel == inputText);
+
+        console.log(result);
+
+        result.forEach(usuario => {
+          html += `<tr>
+          <td>${usuario.empId}</td>
+          <td>${usuario.empFirstname}</td>
+          <td>${usuario.empLastname}</td>
+          <td>${usuario.empSalary}</td>
+          <td>${usuario.empLevel}</td>
+          </tr>`;
+        })
+      } else {
+        (data || []).forEach(usuario => {
+          html += `<tr>
+            <td>${usuario.empId}</td>
+            <td>${usuario.empFirstname}</td>
+            <td>${usuario.empLastname}</td>
+            <td>${usuario.empSalary}</td>
+            <td>${usuario.empLevel}</td>
+          </tr>`;
+        })
+      }
+
+    /*(data || []).forEach(usuario => {
       html += `<tr>
         <td>${usuario.empId}</td>
         <td>${usuario.empFirstname}</td>
@@ -20,7 +51,7 @@ searchBtn.onclick = async function () {
         <td>${usuario.empSalary}</td>
         <td>${usuario.empLevel}</td>
       </tr>`;
-    });
+    });*/
     
     document.querySelector('tbody').innerHTML = html;
     document.getElementById('infoTable').style.display = 'block'
